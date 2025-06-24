89b6fbb767b049ca104bff582b27c5df36bf3f5e
Jack Clarke <jackclarke95@gmail.com>
2025-06-24 22:14:26 +0100

    Login page, sign out button, clean up

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,4 +1,4 @@
-import { PrimaryButton } from "@fluentui/react";
+import { DefaultEffects, PrimaryButton, Stack, Text } from "@fluentui/react";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../lib/firebase";
 
@@ -9,7 +9,32 @@ const Login = () => {
   };
 
   return (
-    <PrimaryButton onClick={handleSignIn}>Sign in with Google</PrimaryButton>
+    <Stack
+      style={{
+        padding: "2rem",
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Stack
+        style={{
+          boxShadow: DefaultEffects.elevation8,
+          padding: "2rem",
+          borderRadius: "8px",
+          backgroundColor: "#fff",
+          maxWidth: "400px",
+          width: "100%",
+        }}
+        tokens={{ childrenGap: 20 }}
+      >
+        <Text>Please sign in to access the meal planner.</Text>
+        <PrimaryButton onClick={handleSignIn}>
+          Sign in with Google
+        </PrimaryButton>
+      </Stack>
+    </Stack>
   );
 };
 
