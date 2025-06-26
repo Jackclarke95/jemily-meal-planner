9d8421689795eafc0ce9a49a1fece0f1f0141be6
Jack Clarke <jackclarke95@gmail.com>
2025-06-26 14:37:05 +0100

    Add unused variable, and make beta workflow ignore unused variable warnings

--- src/Components/Pages/AddLunchPlan.tsx
+++ src/Components/Pages/AddLunchPlan.tsx
@@ -2,6 +2,7 @@ import { Text } from "@fluentui/react";
 import Page from "../Page";
 
 const AddLunchPlans = () => {
+  var foo = "foo";
   return (
     <Page title="Add Lunch Plan" backPath="/">
       <Text>Here you can create your lunch plans.</Text>
