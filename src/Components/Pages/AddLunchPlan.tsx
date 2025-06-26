5d882773aeb5700e1d5eb327ab2084fbe8a98cbc
Jack Clarke <jackclarke95@gmail.com>
2025-06-26 14:57:38 +0100

    Remove unused variable

--- src/Components/Pages/AddLunchPlan.tsx
+++ src/Components/Pages/AddLunchPlan.tsx
@@ -2,7 +2,6 @@ import { Text } from "@fluentui/react";
 import Page from "../Page";
 
 const AddLunchPlans = () => {
-  var foo = "foo";
   return (
     <Page title="Add Lunch Plan" backPath="/">
       <Text>Here you can create your lunch plans.</Text>
